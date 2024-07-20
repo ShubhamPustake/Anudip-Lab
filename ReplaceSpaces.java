0ddf3051fc3c6f80e45c1d5ce2e09a0209e8a98c
Shubham Pustake <114168524+Shuubzz@users.noreply.github.com>
2024-07-20 14:04:46 +0530

    Add files via upload

--- ReplaceSpaces.java
+++ ReplaceSpaces.java
@@ -1,14 +1,31 @@
+/*Write a Java program to replace all spaces in a sentence with #. 
+All continuous spaces should be replaced with only one #. 
+Example: There are   3   spaces. Should become: There#are#3#spaces.*/
 
 public class ReplaceSpaces {
 
     public static void main(String[] args) {
-        String input = "There are   3   spaces.";
-        String output = replaceSpacesWithHash(input);
-        System.out.println(output);
+        String sentence = "There are   3   spaces.";
+        String result = replaceSpacesWithHash(sentence);
+        System.out.println(result);
     }
 
     public static String replaceSpacesWithHash(String sentence) {
-        sentence = sentence.trim();
-        return sentence.replaceAll("\\s+", "#");
+        StringBuilder result = new StringBuilder();
+        boolean inSpace = false;
+
+        for (char ch : sentence.toCharArray()) {
+            if (ch == ' ') {
+                if (!inSpace) {
+                    result.append('#');
+                    inSpace = true;
+                }
+            } else {
+                result.append(ch);
+                inSpace = false;
+            }
+        }
+
+        return result.toString();
     }
 }
